ced581f06736be73f22fb5aaf6c2eb70eed0f23b
afeiship <1290657123@qq.com>
2018-07-20 20:56:24 +0800

    issue: nomralize abs path

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 var glob = require('glob');
+var resolve = require('path').resolve;
+var dirname = require('app-root-path').path;
 var DEFAULT_SLICE_KEYS = [
   'src/modules',
   'index.js'
 ];
 
-function getSlicedKey(inPath, inSliceKeys) {
+
+function getSlicedKey(inPath, inSliceKeys, inOptions) {
+  var isAbsolute = inOptions.absolute || false;
+  var startPath = isAbsolute ? resolve(dirname, sliceKeys[0]) : sliceKeys[0];
   var sliceKeys = inSliceKeys || DEFAULT_SLICE_KEYS;
-  var start = sliceKeys[0].length + 1;
+  var start = startPath.length + 1;
   var end = sliceKeys[1].length + 1;
   return inPath.slice(start, -end);
 }
 
 module.exports = function (inGlob, inSliceKeys, inOptions) {
-  var globPaths = Array.isArray(inGlob) ? inGlob : glob.sync(inGlob, inOptions);
+  var options = inOptions || {};
+  var globPaths = Array.isArray(inGlob) ? inGlob : glob.sync(inGlob, options);
   var entries = {};
   globPaths.forEach(function (path) {
     entries[
-      getSlicedKey(path, inSliceKeys)
+      getSlicedKey(path, inSliceKeys, options)
     ] = path;
   });
   return entries;
