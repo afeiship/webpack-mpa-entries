39638c57187a8ca950c457f249dc5d5e42dd6478
afeiship <1290657123@qq.com>
2018-07-20 22:26:45 +0800

    wip

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var DEFAULT_SLICE_KEYS = [
 
 function getSlicedKey(inPath, inSliceKeys, inOptions) {
   var isAbsolute = inOptions.absolute || false;
-  var startPath = isAbsolute ? resolve(dirname, sliceKeys[0]) : sliceKeys[0];
   var sliceKeys = inSliceKeys || DEFAULT_SLICE_KEYS;
+  var startPath = isAbsolute ? resolve(dirname, sliceKeys[0]) : sliceKeys[0];
   var start = startPath.length + 1;
   var end = sliceKeys[1].length + 1;
   return inPath.slice(start, -end);
@@ -21,8 +21,7 @@ module.exports = function (inGlob, inSliceKeys, inOptions) {
   var globPaths = Array.isArray(inGlob) ? inGlob : glob.sync(inGlob, options);
   var entries = {};
   globPaths.forEach(function (path) {
-    entries[ getSlicedKey(path, inSliceKeys, options) ] = path;
+    entries[getSlicedKey(path, inSliceKeys, options)] = path;
   });
   return entries;
 };
-
